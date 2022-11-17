c45fdd01d5e52043072fd2e1f7afeefa9d5afc09
JKD111995 <jake.duque@gmail.com>
2022-11-17 02:03:03 +0000

    navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,18 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
 	return (
-		
-		// <nav className="navbar navbar-light bg-light mb-3">
-		// 	<Link to="/">
-		// 		<span className="navbar-brand mb-0 h1">Blue Comet Gaming</span>
-		// 	</Link>
-		// 	<div className="ml-auto">
-		// 		<Link to="/demo">
-		// 			<button className="btn btn-primary">Check the Context in action</button>
-		// 		</Link>
-		// 	</div>
-		// </nav>
+		<nav className="navbar navbar-dark bg-dark">
+			<Link to="/">
+				<h1 className="navbar-brand mb-0 mx-2"><i className="fa-solid fa-meteor mx-2"></i>Blue Comet Gaming</h1>
+			</Link>
+				<form className="d-flex">
+					<input className="form-control mr-sm-2" type="search" placeholder="Find Games" aria-label="Search"></input>
+					<button className="btn btn-secondary my-2 my-sm-0" type="submit"><i className="fa-solid fa-magnifying-glass"></i></button>
+				</form>
+			<div className="ml-auto">
+				<p className="text-white text-center">Check out our Github</p>
+				<Link to="/demo">
+					<button className="btn btn-primary mx-2">Steven</button>
+				</Link>
+				<Link to="/demo">
+					<button className="btn btn-primary mx-2">Luis</button>
+				</Link>
+				<Link to="/demo">
+					<button className="btn btn-primary mx-2">Jacob</button>
+				</Link>
+			</div>
+		</nav>
 	);
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -5,7 +5,7 @@ import { testAPI } from "../../../TestApi";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<button onClick={testAPI}>Get APi</button>
+		{/* <button onClick={testAPI}>Get APi</button> */}
 		<h1>Final Project</h1>
 		<p>Filler text</p>
 		<a href="#" className="btn btn-success">
