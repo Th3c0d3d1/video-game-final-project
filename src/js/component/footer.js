45715acf5ec9b467ac724b9e1e840f289cff1f60
JKD111995 <jake.duque@gmail.com>
2022-11-19 01:36:35 +0000

    footer mods

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import "../../styles/footer.css";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+  <body className="d-flex flex-column">
+    <footer className="flex-shrink-0 py-4 bg-dark text-white-50 mt-auto">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-10">
+            <h3>Blue Comet Gaming</h3>
+          </div>
+          <div class="col-sm-2">
+            <a href="#" class="link-secondary text-decoration-none">
+              <h5>About Us</h5>
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </body>
 );
