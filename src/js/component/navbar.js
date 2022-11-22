039400b19b2d6249b44e13ca39be03f50e83177f
JKD111995 <jake.duque@gmail.com>
2022-11-22 01:44:55 +0000

    github links

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,27 +3,44 @@ import { Link } from "react-router-dom";
 import "../../styles/navbar.css";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-dark bg-dark">
-			<Link to="/">
-				<h2 className="navbar-brand mb-0 mx-2"><h2><i className="fa-solid fa-meteor mx-2"></i>Blue Comet Gaming</h2></h2>
-			</Link>
-				<form className="d-flex">
-					<input className="form-control mr-sm-2" type="search" placeholder="Find Games" aria-label="Search"></input>
-					<button className="btn btn-secondary my-2 my-sm-0" type="submit"><i className="fa-solid fa-magnifying-glass"></i></button>
-				</form>
-			<div className="ml-auto">
-				<p className="text-white text-center">Check out our Github</p>
-				<Link to="/demo">
-					<button className="btn btn-primary mx-2">Steven</button>
-				</Link>
-				<Link to="/demo">
-					<button className="btn btn-primary mx-2">Luis</button>
-				</Link>
-				<Link to="/demo">
-					<button className="btn btn-primary mx-2">Jacob</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container">
+        <Link to="/">
+            <h2 className="text-decoration-none link-light">
+              <i className="fa-solid fa-meteor mx-2"></i>Blue Comet Gaming
+            </h2>
+        </Link>
+        <form className="d-flex">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Find Games"
+            aria-label="Search"
+          ></input>
+          <button className="btn btn-secondary my-2 my-sm-0" type="submit">
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </form>
+        <div className="ml-auto">
+          <p className="text-white text-center">Check out our Github</p>
+          {/* <Link to="/demo">
+            <button className="btn btn-primary mx-2">Steven</button>
+          </Link> */}
+          <button className="btn btn-primary mx-2">
+            <a href="https://www.github.com/Th3c0d3d1/" className="text-decoration-none link-light">Steven</a>
+          </button>
+          <button className="btn btn-primary mx-2">
+            <a href="https://www.github.com/ItsLuisT/" className="text-decoration-none link-light">Luis</a>
+          </button>
+          <button className="btn btn-primary mx-2">
+            <a href="https://www.github.com/JKD111995/" className="text-decoration-none link-light">Jacob</a>
+          </button>
+          {/* <Link to="/demo">
+            <button className="btn btn-primary mx-2">Jacob</button>
+          </Link> */}
+        </div>
+      </div>
+    </nav>
+  );
 };
