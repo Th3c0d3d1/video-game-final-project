617adbe0ca99b9a093540d2e4e4514c2b69f9e15
JKD111995 <jake.duque@gmail.com>
2022-11-22 01:49:24 +0000

    new api key

--- TestApi.js
+++ TestApi.js
@@ -1,5 +1,5 @@
 export function testAPI(){
-	fetch("https://api.rawg.io/api/games?key=bfe242c109884e06860a295ba9f3a547")
+	fetch("https://api.rawg.io/api/games?key=bfe242c109884e06860a295ba9f3a547&page_size=40")
 .then((response)=> response.json())
 .then((data)=>{
     console.log(data.results)
