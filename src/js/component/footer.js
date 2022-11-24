622517ccb01b967d8c1dc7d2a53322435b8a126b
JKD111995 <jake.duque@gmail.com>
2022-11-24 01:49:43 +0000

    Added About Us section

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,21 +1,24 @@
 import React, { Component } from "react";
 import "../../styles/footer.css";
+import { Link } from "react-router-dom";
 
 export const Footer = () => (
-  <body className="d-flex flex-column">
-    <footer className="flex-shrink-0 py-4 bg-dark text-white-50 mt-auto">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-10">
-            <h3>Blue Comet Gaming</h3>
-          </div>
-          <div class="col-sm-2">
-            <a href="#" class="link-secondary text-decoration-none">
+  // <body className="d-flex flex-column footer">
+  <footer className="flex-shrink-0 py-4 bg-dark text-white-50 mt-auto">
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-10">
+          <h3>Blue Comet Gaming</h3>
+        </div>
+        <div className="col-sm-2">
+          <Link to="/aboutUs">
+            <button className="btn btn-primary">
               <h5>About Us</h5>
-            </a>
-          </div>
+            </button>
+          </Link>
         </div>
       </div>
-    </footer>
-  </body>
+    </div>
+  </footer>
+  // </body>
 );
