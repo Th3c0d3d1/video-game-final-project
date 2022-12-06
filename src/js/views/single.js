35380171a675ff5cbe0f007a3b3244a694681648
Th3c0d3d1 <bittradeguy007@gmail.com>
2022-12-05 19:19:53 -0500

    merging again

--- src/js/views/single.js
+++ src/js/views/single.js
@@ -4,20 +4,40 @@ import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const Single = props => {
+	const {theid} = useParams();
 	const { store, actions } = useContext(Context);
 	const params = useParams();
+
+	useEffect (() => {
+		fetch(
+			`https://api.rawg.io/api/games?key=bfe242c109884e06860a295ba9f3a547&search=${theid}&search_exact=true`
+		  )
+			.then(
+			  (response) => response.json()
+			  // converts json into js
+			)
+			.then((responseData) => {
+			  // setResult(responseData.results);
+			  setGame(responseData.results[0]); // responseData.results <--- the slug
+			  // console.log(responseData.results);
+			  // console.log(store.results)
+			})
+	}, []) 
+	// console.log(theid);
+
+	const [game, setGame]= useState({name: 'FirstGame'});
+	console.log('example', game.name);
+
 	return (
-		<div className="jumbotron">
-			<h1 className="display-4">This will show the demo element: {store.demo[params.theid].title}</h1>
-
-			<hr className="my-4" />
-
-			<Link to="/">
-				<span className="btn btn-primary btn-lg" href="#" role="button">
-					Back home
-				</span>
-			</Link>
-		</div>
+		<p>{game.name}</p>
+		// <div className="searchEntry">
+		// 	<div className="">
+		// 		<img src={result.background_image} className="img-thumbnail rounded" />
+		// 	</div>
+		// 	<div className="">
+		// 		{result.name}
+		// 	</div>
+		// </div>
 	);
 };
 
--- src/layout.js
+++ src/layout.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
+import { AboutUs } from "./views/aboutUs";
 import { Single } from "./views/single";
 import injectContext from "./store/appContext";
 
@@ -25,8 +25,8 @@ const Layout = () => {
 						<Route exact path="/">
 							<Home />
 						</Route>
-						<Route exact path="/demo">
-							<Demo />
+						<Route exact path="/AboutUs">
+							<AboutUs />
 						</Route>
 						<Route exact path="/single/:theid">
 							<Single />
